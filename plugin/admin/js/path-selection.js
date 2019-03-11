826a6545bd562bf13be9c331c1630660b1f8d803
Kristian Kosorin <456620@fi.muni.cz>
2019-03-11 22:34:15 +0100

    Ajax testing

--- plugin/admin/js/path-selection.js
+++ plugin/admin/js/path-selection.js
@@ -1,17 +1,25 @@
 jQuery( document ).ready( function( $ ) {
-  function listGdriveDir( path ) {
-		$( '#sgdd_root_selection_body' ).html( '' );
-		$( '#submit' ).attr( 'disabled', 'disabled' );
-		$.get( sgdgRootpathLocalize.ajax_url, {
-			_ajax_nonce: sgdgRootpathLocalize.nonce, // eslint-disable-line camelcase
-			action: 'list_gdrive_dir',
-			path: path
-			}, function( data ) {
-				if ( data.directories ) {
-					success( path, data );
-				} else if ( data.error ) {
-					error ( data.error );
+	function showDir( path ) {
+			$.ajax({ 
+				url: sgddRootPathLocalize.ajax_url,
+				type: "GET",
+				data: {
+					action: 'listDrive',
+					path: path,
+					_ajax_nonce: sgddRootPathLocalize.nonce
+				},
+				beforeSend: function() {
+					$( '#loadingCircle' ).fadeIn();
+				},
+				success: function( response ) {	
+					$( '#loadingCircle' ).fadeOut();
+					$( '#rootPath' ).fadeIn();
+				},
+				error: function() {
+					alert("Error");
 				}
 			});
-}
+	}
+
+	showDir( sgddRootPathLocalize.path );
 });
