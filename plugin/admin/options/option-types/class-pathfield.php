2a3957c64b294bfe4c70aa075da4764374d60376
Kristián Kosorín <xkosorin@fi.muni.cz>
2019-03-18 20:04:05 +0100

    WPCS & PHPCompatibility

--- plugin/admin/options/option-types/class-pathfield.php
+++ plugin/admin/options/option-types/class-pathfield.php
@@ -5,7 +5,15 @@ require_once __DIR__ . '/class-settingfield.php';
 
 class PathField extends SettingField {
 	public function register() {
-		register_setting( $this->page, $this->id, [ 'type' => 'string', 'sanitize_callback' => [ $this, 'sanitize' ], 'default' => $this->default_value ] );
+		register_setting(
+			$this->page,
+			$this->id,
+			[
+				'type'              => 'string',
+				'sanitize_callback' => [ $this, 'sanitize' ],
+				'default'           => $this->default_value,
+			]
+		);
 	}
 
 	public function sanitize( $value ) {
