8774a94ac5265ce8e192bfe3b568ebcb7a2a1518
Kristián Kosorín <xkosorin@fi.muni.cz>
2019-06-20 22:07:00 +0200

    Added option to list folder as list or grid

--- plugin/public/block.php
+++ plugin/public/block.php
@@ -14,9 +14,10 @@ function register() {
 function add_block() {
 	\Sgdd\enqueue_script( 'sgdd_block_js', '/public/js/block.js', [ 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'sgdd_file_selection_js' ] );
 	\Sgdd\enqueue_script( 'sgdd_file_selection_js', '/public/js/file-selection.js', [ 'wp-components', 'wp-element', 'sgdd_inspector_js', 'sgdd_settings_base_js' ] );
-	\Sgdd\enqueue_script( 'sgdd_inspector_js', '/public/js/inspector.js', [ 'wp-element', 'sgdd_integer_settings_js' ] );
+	\Sgdd\enqueue_script( 'sgdd_inspector_js', '/public/js/inspector.js', [ 'wp-element', 'sgdd_integer_settings_js', 'sgdd_select_settings_js' ] );
 	\Sgdd\enqueue_script( 'sgdd_settings_base_js', '/public/js/settings-base.js', [ 'wp-element' ] );
 	\Sgdd\enqueue_script( 'sgdd_integer_settings_js', '/public/js/integer-setting.js', [ 'wp-element', 'sgdd_settings_base_js' ] );
+	\Sgdd\enqueue_script( 'sgdd_select_settings_js', '/public/js/select-setting.js', [ 'wp-element', 'sgdd_settings_base_js' ] );
 	wp_enqueue_script( 'thickbox' );
 	wp_enqueue_style( 'thickbox' );
 
@@ -32,13 +33,15 @@ function add_block() {
 			'rootPath'         => \Sgdd\Admin\Options\Options::$root_path->get(),
 			'embedWidth'       => [ esc_html__( 'Width', 'skaut-google-drive-documents' ), \Sgdd\Admin\Options\Options::$embed_width->get() ],
 			'embedHeight'      => [ esc_html__( 'Height', 'skaut-google-drive-documents' ), \Sgdd\Admin\Options\Options::$embed_height->get() ],
-			'folder'           => [ esc_html__( 'List folder', 'skaut-google-drive-documents' ), 'false' ],
+			'list'             => esc_html__( 'List', 'skaut-google-drive-documents' ),
+			'grid'             => esc_html__( 'Grid', 'skaut-google-drive-documents' ),
+			'folderType'       => [ esc_html__( 'List folder as', 'skaut-google-drive-documents' ) ],
 		]
 	);
 
 	\Sgdd\enqueue_style( 'sgdd_block', '/public/css/block.css' );
 	register_block_type(
-		'sakut-google-drive-documents/block',
+		'skaut-google-drive-documents/block',
 		[
 			'editor_script'   => 'sgdd_block_js',
 			'render_callback' => '\\Sgdd\\Pub\\Block\\display',
@@ -50,24 +53,28 @@ function add_block() {
 function display( $attr ) {
 	$size = '';
 
-	if ( isset( $attg['embedWidth'] ) ) {
+	if ( isset( $attr['embedWidth'] ) ) {
 		$size .= 'width:' . $attr['embedWidth'] . 'px;';
 	}
 
 	if ( isset( $attr['embedHeight'] ) ) {
-		$size .= 'height:' . $attr['embedWidth'] . 'px;';
+		$size .= 'height:' . $attr['embedHeight'] . 'px;';
 	}
-
-	$test   = print_file( $attr['fileId'] );
 	
-	if ( !isset( $test['id'] ) ) {
-		$result = 'Niekde nastala chyba!' . $test;
-	} else if ( isset( $attr['folder'] ) && 'true' === $attr['folder'] ) {
+	if ( isset( $attr['folderType'] ) ) {
+		if ( 'list' === $attr['folderType'] ) {
+			$type = '#list';
+		} else {
+			$type = '#grid';
+		}
+
+		$test = print_file( $attr['folderId'] );
 		$link   = $test['id'];
-		$result = '<iframe src="https://drive.google.com/embeddedfolderview?id=' . $link . '#list" style="' . $size . ' border:0;"></iframe>';
+		$result = '<iframe src="https://drive.google.com/embeddedfolderview?id=' . $link . $type . '" style="' . $size . ' border:0;"></iframe>' . $attr['folderId'] . $attr['folderType'];
 	} else {
+		$test = print_file( $attr['fileId'] );
 		$link   = $test['id'];
-		$result = '<iframe src="https://docs.google.com/viewer?srcid=' . $link . '&pid=explorer&efh=false&a=v&chrome=false&embedded=true" style="' . $size . ' border:0;"></iframe>';
+		$result = '<iframe src="https://docs.google.com/viewer?srcid=' . $link . '&pid=explorer&efh=false&a=v&chrome=false&embedded=true" style="' . $size . ' border:0;"></iframe>' . $attr['fileId'] . $attr['folderType'];
 	}
 
 	return $result;
--- plugin/public/js/select-setting.js
+++ plugin/public/js/select-setting.js
@@ -8,18 +8,18 @@ SgddSelectSetting.prototype = Object.create( SgddSettingsBase.prototype );
 SgddSelectSetting.prototype.renderInput = function() {
 	var that = this;
 	var value = this.block.getAttribute( this.name );
-	return el( 'div', {className: 'sgdd-block-units'}, [
-		el( 'label', {className: 'sgdd-block-settings-radio', for: this.name + '_px'}, [
-			el( 'input', {checked: 'pixels' === this.state.value, disabled: undefined === value, id: this.name + '_px', name: this.name, onChange: function( e ) {
+	return el( 'div', {className: 'sgdd-block-folder-type'}, [
+		el( 'label', {className: 'sgdd-block-settings-radio', for: this.name + '_list'}, [
+			el( 'input', {checked: 'list' === this.state.value, disabled: undefined === value, id: this.name + '_list', name: this.name, onChange: function( e ) {
 			that.change( e );
-		}, type: 'radio', value: 'pixels'}),
-			sgddBlockJsLocalize.pixels
+		}, type: 'radio', value: 'list'}),
+			sgddBlockJsLocalize.list
 		]),
-		el( 'label', {className: 'sgdd-block-settings-radio', for: this.name + '_per'}, [
-			el( 'input', {checked: 'percentage' === this.state.value, disabled: undefined === value, id: this.name + '_per', name: this.name, onChange: function( e ) {
+		el( 'label', {className: 'sgdd-block-settings-radio', for: this.name + '_grid'}, [
+			el( 'input', {checked: 'grid' === this.state.value, disabled: undefined === value, id: this.name + '_grid', name: this.name, onChange: function( e ) {
 			that.change( e );
-		}, type: 'radio', value: 'percentage'}),
-			sgddBlockJsLocalize.percentage
+		}, type: 'radio', value: 'grid'}),
+			sgddBlockJsLocalize.grid
 		])
 	]);
 };
